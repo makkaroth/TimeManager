9ff7876936bbb8a032e5f37af7abbb49aa343d8f
Arón <Arn@Makkaroth>
2015-09-15 14:36:39 -0700

    new repository

---
+++ src/timemanager/TimeManager.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timemanager;
+
+/**
+ *
+ * @author Arón Vargas Hernández <aronvargas83@gmail.com>
+ */
+public class TimeManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
